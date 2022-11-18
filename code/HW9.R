33607a4f8d1bfbc34fec40b3413913c66daf1b6d
Ian Hart <ian.hart167@gmail.com>
2022-11-18 13:39:19 -0600

    Q1

--- code/HW9.R
+++ code/HW9.R
@@ -1,10 +1,19 @@
 # Zoo955 HW 9 Ian Hart & Riley Book
+require(ggplot2); require(car)
+
 
 r = 0.2
 K = 100
 time = seq(1, 50)
-N=array(dim=c(1,length(time))); N[1]=5
+N=array(dim=c(length(time), 1)); N[1]=5
 
 for(i in 2:length(time)){
-  N[i] = 
+  N[i] = N[i-1] + r*N[i-1]*(1-N[i-1]/K)
 }
+
+#Q1
+plot(time, N)
+
+#Q2
+
+m1 = nls(N~time)
